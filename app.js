95c608e29132326a14e5f5dc246318f9f21fff28
khaya05 <khayamnyandu5@gmail.com>
2023-05-19 08:08:37 +0200

    unhandled route middleware

--- app.js
+++ app.js
@@ -7,14 +7,20 @@ const userRouter = require('./Routes/userRoutes');
 const app = express();
 
 // MIDDLEWARE
-if (process.env.NODE_ENV === "development")
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 //  ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
