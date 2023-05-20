a107ca7387c5a6a04d56fc7189a70dadf5c903fd
khaya05 <khayamnyandu5@gmail.com>
2023-05-20 05:57:22 +0200

    complete error handling

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -1,9 +1,67 @@
+const AppError = require('../utils/AppError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.keyValue.name;
+  const message = `Duplicate field value: ${value}. PLease use another value!`;
+  return new AppError(message, 400);
+};
+
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  // Programming or other unknown error: don't leak error details
+  else {
+    // 1 Log error
+    // console.error('ERROR!!', err);
+
+    // send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'validationError') error = handleValidationErrorDB(error)
+
+    sendErrorProd(error, res);
+  }
 };
--- Models/TourModel.js
+++ Models/TourModel.js
@@ -1,94 +1,79 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
     name: {
       type: String,
+      required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
-      required: [true, 'A tour must have a name'],
-      maxLength: [40, 'A tour name must not me more than 40 characters'],
-      minLength: [3, 'A tour name must not me less than 3 characters'],
-      // validate: [validator.isAlpha, "Tour name must only contain characters"],
+      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      minlength: [10, 'A tour name must have more or equal then 10 characters'],
+      // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
-
     slug: String,
-
     duration: {
       type: Number,
-      required: [true, 'A tour must have duration'],
+      required: [true, 'A tour must have a duration'],
     },
-
     maxGroupSize: {
       type: Number,
-      required: [true, 'A tour must have group size'],
+      required: [true, 'A tour must have a group size'],
     },
-
     difficulty: {
       type: String,
-      required: [true, 'A tour must have difficulty'],
+      required: [true, 'A tour must have a difficulty'],
       enum: {
         values: ['easy', 'medium', 'difficult'],
-        message: "Difficulty can only be 'easy', 'medium' or 'difficult' ",
+        message: 'Difficulty is either: easy, medium, difficult',
       },
     },
-
-    ratingAverage: {
+    ratingsAverage: {
       type: Number,
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
-      max: [5, 'Rating must be 5 or below'],
+      max: [5, 'Rating must be below 5.0'],
     },
-
     ratingsQuantity: {
       type: Number,
       default: 0,
     },
-
     price: {
-      required: [true, 'a tour must have a price'],
       type: Number,
+      required: [true, 'A tour must have a price'],
     },
-
     priceDiscount: {
       type: Number,
       validate: {
         validator: function (val) {
-          // this only points to the current doc on new document creation
+          // this only points to current doc on NEW document creation
           return val < this.price;
         },
         message: 'Discount price ({VALUE}) should be below regular price',
       },
     },
-
     summary: {
       type: String,
       trim: true,
-      required: [true, 'A tour must have a summary'],
+      required: [true, 'A tour must have a description'],
     },
-
     description: {
       type: String,
       trim: true,
     },
-
     imageCover: {
       type: String,
       required: [true, 'A tour must have a cover image'],
     },
-
     images: [String],
-
     createdAt: {
       type: Date,
       default: Date.now(),
       select: false,
     },
-
     startDates: [Date],
-
     secretTour: {
       type: Boolean,
       default: false,
@@ -100,36 +85,45 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
-// virtual property
 tourSchema.virtual('durationWeeks').get(function () {
-  return Math.floor(this.duration / 7);
+  return this.duration / 7;
 });
 
-// document middleware: runs before .save() || .create()
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
 
-// tourSchema.post('save', function (doc, next) {
-//   console.log(doc)
+// tourSchema.pre('save', function(next) {
+//   console.log('Will save document...');
+//   next();
+// });
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
 //   next();
 // });
 
 // QUERY MIDDLEWARE
+// tourSchema.pre('find', function(next) {
 tourSchema.pre(/^find/, function (next) {
-  this.find({ secretTout: { $ne: true } });
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
   next();
 });
 
-// tourSchema.post(/^find/, function (docs, next) {
-//   console.log('middleware executed');
-//   next();
-// })
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
 
 // AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function (next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  console.log(this.pipeline());
   next();
 });
 
