22f9ae3fefd63301fe6a3963c1bb457203a40f17
khaya05 <khayamnyandu5@gmail.com>
2023-05-20 06:20:33 +0200

    end section 9

--- server.js
+++ server.js
@@ -2,9 +2,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
-
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -20,9 +25,16 @@ mongoose
   })
   .then((con) => console.log('DB connection successful'));
 
-
 // START SERVER
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`app running on port ${port}...`);
 });
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+})
