9e5054f277562f5055ab69a1132aebec903b871d
khaya05 <khayamnyandu5@gmail.com>
2023-05-29 17:16:01 +0200

    implemented getAllReviews

--- Routes/reviewRoutes.js
+++ Routes/reviewRoutes.js
@@ -3,12 +3,13 @@ const {
   getAllReviews,
   createReview
 } = require('../controllers/reviewController');
+const { protect, restrictTo } = require('../controllers/authController');
 
 const router = express.Router();
 
 router
-  .route('/reviews')
-  .get(getAllReviews)
-  .post(createReview);
+  .route('/')
+  .get(protect, restrictTo('user'), getAllReviews)
+  .post(protect, restrictTo('user'), createReview);
 
 module.exports = router;
