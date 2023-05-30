0a5b71dc11ddfabd550a11d10c2d37527df542dd
khaya05 <khayamnyandu5@gmail.com>
2023-05-30 14:51:15 +0200

    imported data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -7,13 +7,13 @@ const Review = require('./../../models/reviewModel');
 
 dotenv.config({ path: '../../config.env' });
 
-// const DB = process.env.DATABASE.replace(
-//   '<PASSWORD>',
-//   process.env.DATABASE_PASSWORD
-// );
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose
-  .connect(process.env.DATABASE_, {
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
@@ -32,7 +32,7 @@ const reviews = JSON.parse(
 const importData = async () => {
   try {
     await Tour.create(tours);
-    await User.create(users);
+    await User.create(users, { validateBeforeSave: false });
     await Review.create(reviews);
     console.log('Data successfully loaded!');
   } catch (err) {
