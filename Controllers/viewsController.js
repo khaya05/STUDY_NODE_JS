3c3943a3818049a767bfdf4fff9e4c509bf6b20c
khaya05 <khayamnyandu5@gmail.com>
2023-06-02 14:51:44 +0200

    Remove bundle.js and bundle.js.map files

--- Controllers/viewsController.js
+++ Controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const asyncWrapper = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getOverview = asyncWrapper(async (req, res, next) => {
   // 1) get tour data from collection
@@ -16,6 +17,10 @@ exports.getTour = asyncWrapper(async (req, res, next) => {
     path: 'reviews',
     fields: 'review rating user'
   });
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
 
   res.status(200).render('tour', {
     title: `${tour.name} Tour`,
