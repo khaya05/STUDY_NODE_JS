588779298f239c98594097a63bb3936b5ff65e3e
khaya05 <khayamnyandu5@gmail.com>
2023-06-02 13:26:00 +0200

    logged out users

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,12 @@
 /*eslint-disable */
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
-import { login } from './login';
+import { login, logout } from './login';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form');
+const logOutBtn = document.querySelector('.nav__el--logout');
 
 if (mapBox) {
   const locations = JSON.parse(mapBox.dataset.locations);
@@ -20,3 +21,5 @@ if (loginForm) {
     login(email, password);
   });
 }
+
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
